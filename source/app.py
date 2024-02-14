72d439b622cfe690c821203c0698d0e100d42eec
semyon <semen.ryaboff@yandex.ru>
2024-02-14 17:24:13 +0300

    Instant Messages - flash, get_flashed_messages

--- source/app.py
+++ source/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request
+import os
+
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(12).hex()
 
 menu = [{"name": "Установка", "url": "install-flask"},
         {"name": "Первое приложение", "url": "first-app"},
@@ -25,7 +28,11 @@ def profile(username):
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
     if request.method == 'POST':
-        print(request.form)
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
+
     return render_template('contact.html', title='Обратная связь', menu=menu)
 
 
