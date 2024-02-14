fa8eb9f7b0d4dd283799a89366705a70e4a728ca
semyon <semen.ryaboff@yandex.ru>
2024-02-14 17:44:30 +0300

    The errorhandler decorator, redirect and abort functions

--- source/app.py
+++ source/app.py
@@ -1,11 +1,12 @@
 import os
 
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, session, redirect, url_for, abort
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(12).hex()
 
-menu = [{"name": "Установка", "url": "install-flask"},
+menu = [{"name": "Главная страница", "url": "/"},
+        {"name": "Установка", "url": "install-flask"},
         {"name": "Первое приложение", "url": "first-app"},
         {"name": "Обратная связь", "url": "contact"}]
 
@@ -22,6 +23,9 @@ def about():
 
 @app.route("/profile/<username>")
 def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+
     return f"Пользователь: {username}"
 
 
@@ -34,6 +38,27 @@ def contact():
             flash('Ошибка отправки', category='error')
 
     return render_template('contact.html', title='Обратная связь', menu=menu)
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == "POST" and request.form['username'] == "selfedu" and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title="Авторизация", menu=menu)
+
+
+@app.errorhandler(404)
+def page_not_fount404(error):
+    return render_template('page404.html', title="Страница не найдена", menu=menu), 404
+
+
+@app.errorhandler(401)
+def page_not_fount401(error):
+    return render_template('page404.html', title="Нет доступа", menu=menu), 401
 
 
 if __name__ == '__main__':
